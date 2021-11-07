a36e16889cd652794a8785081b4f9eba7f271022
jcprice12 <john.c.price12@gmail.com>
2021-11-07 18:50:47 -0500

    basic retry decorator

--- src/retry.decorator.test.ts
+++ src/retry.decorator.test.ts
@@ -1,12 +1,122 @@
-import { hello } from '.';
+import { Retry } from '.';
 
-describe('When foo', () => {
-  let consoleLogSpy: jest.SpyInstance;
+describe('Retry Decorator', () => {
+  interface ArbitraryArg {
+    foo: string;
+  }
+  interface ArbitraryResult {
+    bar: string;
+  }
+  let arbitraryError: Error;
+  let arbitraryArg: ArbitraryArg;
+  let arbitraryResult: ArbitraryResult;
   beforeEach(() => {
-    consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
-    hello('world');
+    arbitraryError = new Error('errr');
+    arbitraryArg = {
+      foo: 'hello'
+    };
+    arbitraryResult = {
+      bar: 'hi'
+    };
   });
-  test('Then bar', () => {
-    expect(consoleLogSpy).toHaveBeenCalledWith('world');
+
+  describe('Given a class with a method to retry that is not asynchronous', () => {
+    let testMock: jest.Mock<ArbitraryResult>;
+    class TestClass {
+      @Retry()
+      test(arg1: ArbitraryArg): ArbitraryResult {
+        return testMock(arg1);
+      }
+    }
+
+    function assertMethodCalledCorrectly(
+      times: number,
+      arg: ArbitraryArg
+    ): void {
+      expect(testMock).toHaveBeenCalledTimes(times);
+      for (let i = 0; i < times; i++) {
+        expect(testMock).toHaveBeenNthCalledWith(i + 1, arg);
+      }
+    }
+
+    let testable: TestClass;
+    beforeEach(() => {
+      testMock = jest.fn();
+      testable = new TestClass();
+    });
+
+    describe('Given decorated method will not fail', () => {
+      beforeEach(() => {
+        testMock.mockReturnValue(arbitraryResult);
+      });
+
+      describe('When method is invoked with arbitrary arguments', () => {
+        let result: ArbitraryResult;
+        beforeEach(() => {
+          result = testable.test(arbitraryArg);
+        });
+
+        test('Then result is successfully returned', () => {
+          expect(result).toBe(arbitraryResult);
+        });
+
+        test('Then the original method is only called once with the original arguments', () => {
+          assertMethodCalledCorrectly(1, arbitraryArg)
+        });
+      });
+    });
+
+    describe('Given decorated method will always fail', () => {
+      beforeEach(() => {
+        testMock.mockImplementation(() => {
+          throw arbitraryError;
+        });
+      });
+
+      describe('When method is invoked with arbitrary arguments', () => {
+        let thrownError: unknown;
+        beforeEach(() => {
+          try {
+            testable.test(arbitraryArg);
+            fail('an error should have been thrown but was not');
+          } catch (e) {
+            thrownError = e;
+          }
+        });
+
+        test('Then last error thrown by original method is thrown', () => {
+          expect(thrownError).toBe(arbitraryError);
+        });
+
+        test('Then the original method is retried the default number of times', () => {
+          assertMethodCalledCorrectly(3, arbitraryArg)
+        });
+      });
+    });
+
+    describe('Given decorated method will eventually succeed', () => {
+      beforeEach(() => {
+        testMock
+          .mockImplementationOnce(() => {
+            throw arbitraryError;
+          })
+          .mockReturnValue(arbitraryResult);
+      });
+
+      describe('When method is invoked with arbitrary arguments', () => {
+        let result: ArbitraryResult;
+        beforeEach(() => {
+          result = testable.test(arbitraryArg);
+        });
+
+        test('Then result is successfully returned', () => {
+          expect(result).toBe(arbitraryResult);
+        });
+
+        test('Then the original method is called the correct number of times', () => {
+          assertMethodCalledCorrectly(2, arbitraryArg)
+        });
+      });
+    });
   });
 });
--- src/retry.decorator.ts
+++ src/retry.decorator.ts
@@ -1,3 +1,31 @@
-export function hello(world: string) {
-  console.log(world);
+export interface RetryDecoratorOptions {
+  retryAttempts: number;
+}
+
+export function Retry(
+  options: RetryDecoratorOptions = {
+    retryAttempts: 2
+  }
+) {
+  return function (
+    _target: any,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    descriptor.value = new Proxy(descriptor.value, {
+      apply: function (original, thisArg: any, args: any[]) {
+        function retryable(attemptsLeft: number, lastError: unknown): unknown {
+          if (attemptsLeft <= 0) {
+            throw lastError;
+          }
+          try {
+            return original.apply(thisArg, args);
+          } catch (e) {
+            return retryable(attemptsLeft - 1, e);
+          }
+        }
+        return retryable(options.retryAttempts + 1, null);
+      }
+    });
+  };
 }
